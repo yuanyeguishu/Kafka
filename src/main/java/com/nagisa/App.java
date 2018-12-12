9f70182e42b02b5ef439e8f2f5b0844056c76b68
Nagisa <3094589063@qq.com>
2018-12-13 00:30:47 +0800

    add !

--- src/main/java/com/nagisa/App.java
+++ src/main/java/com/nagisa/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         for (int i = 0; i < 1; i++) {
-            System.out.println( "Hello World!" );
+            System.out.println( "Hello World!!" );
         }
     }
 }
