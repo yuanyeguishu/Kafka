c4088f5a15b041de4acdeadb4b9a83c2eaab933b
Nagisa <3094589063@qq.com>
2018-12-13 00:28:52 +0800

    loop

--- src/main/java/com/nagisa/App.java
+++ src/main/java/com/nagisa/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for (int i = 0; i < 1; i++) {
+            System.out.println( "Hello World!" );
+        }
     }
 }
